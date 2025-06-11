6af363d48cbe626a6d2d5f62ea4b6ebc1c05c791
Monika <mnagireddy@uco.edu>
2025-06-11 14:39:18 -0500

    Register and login code commit

---
+++ todo-management/src/main/java/net/javaguides/todo_management/controller/AuthController.java
@@ -0,0 +1,35 @@
+package net.javaguides.todo_management.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.todo_management.dto.LoginDto;
+import net.javaguides.todo_management.dto.RegisterDto;
+import net.javaguides.todo_management.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private AuthService authService;
+
+    //Build Register REST API
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
+        String response=authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+
+    }
+
+    //Build login REST API
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+        String response=authService.login(loginDto);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
--- todo-management/src/main/java/net/javaguides/todo_management/controller/ToDoManagementController.java
+++ todo-management/src/main/java/net/javaguides/todo_management/controller/ToDoManagementController.java
@@ -5,22 +5,27 @@ import net.javaguides.todo_management.dto.ToDoManagementDto;
 import net.javaguides.todo_management.service.ToDoManagementService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("api/todos")
 @AllArgsConstructor
 public class ToDoManagementController {
     private ToDoManagementService toDoManagementService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     //Build AddToDo REST API
     @PostMapping
     public ResponseEntity<ToDoManagementDto> addToDo(@RequestBody ToDoManagementDto todo){
         ToDoManagementDto newToDo=toDoManagementService.addToDo(todo);
         return new ResponseEntity<>(newToDo, HttpStatus.CREATED);
     }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
 //Build getToDo REST API
     @GetMapping("{id}")
     public ResponseEntity<ToDoManagementDto> getToDo(@PathVariable("id") Long taskId){
@@ -28,6 +33,7 @@ public class ToDoManagementController {
         return new ResponseEntity<>(toDoDto,HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     //Build Get AllToDo tasks REST API
 @GetMapping
     public ResponseEntity<List<ToDoManagementDto>> getAllToDos(){
@@ -35,6 +41,7 @@ public class ToDoManagementController {
         return ResponseEntity.ok(toDoDtoList);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     //Build UpdateToDo REST API
     @PutMapping("{id}")
     public ResponseEntity<ToDoManagementDto> updateToDo(@PathVariable("id") Long taskId,@RequestBody ToDoManagementDto toDoManagementDto){
@@ -42,6 +49,7 @@ public class ToDoManagementController {
         return ResponseEntity.ok(updateToDoDto);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     //Build DeleteToDo REST API
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteToDo(@PathVariable("id") Long taskId){
@@ -49,6 +57,7 @@ public class ToDoManagementController {
         return ResponseEntity.ok("Task deleted successfully!");
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     //Build CompleteToDo REST API
     @PatchMapping("{id}/complete")
     public ResponseEntity<ToDoManagementDto> completeToDo(@PathVariable("id") Long taskId){
@@ -56,6 +65,7 @@ public class ToDoManagementController {
         return ResponseEntity.ok(updatedToDo);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     //Build IncompleteToDo REST API
     @PatchMapping("{id}/in-complete")
     public ResponseEntity<ToDoManagementDto> inCompleteToDo(@PathVariable("id") Long taskId){
